18fde84ffb50432eaf91db1ca8e4c9545829e079
Clark Winkelmann <clark.winkelmann@gmail.com>
2022-04-01 18:24:48 +0200

    Webpack 5

--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -3,8 +3,6 @@ import app from 'flarum/forum/app';
 import UserCard from 'flarum/forum/components/UserCard';
 import icon from 'flarum/common/helpers/icon';
 
-/* global m */
-
 app.initializers.add('clarkwinkelmann-likes-received', () => {
     extend(UserCard.prototype, 'infoItems', function (items) {
         const likes = this.attrs.user.attribute('likesReceived');
