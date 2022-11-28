646492088fb3f3172c87ef3d3264a6803e1e76cd
Clark Winkelmann <clark.winkelmann@gmail.com>
2022-11-28 23:51:33 +0100

    Update dependencies and typings

--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -2,10 +2,11 @@ import {extend} from 'flarum/common/extend';
 import app from 'flarum/forum/app';
 import UserCard from 'flarum/forum/components/UserCard';
 import icon from 'flarum/common/helpers/icon';
+import User from 'flarum/common/models/User';
 
 app.initializers.add('clarkwinkelmann-likes-received', () => {
     extend(UserCard.prototype, 'infoItems', function (items) {
-        const likes = this.attrs.user.attribute('likesReceived');
+        const likes = ((this.attrs as any).user as User).attribute('likesReceived');
 
         if (Number.isInteger(likes)) {
             items.add('likesReceived', m('span.UserCard-likesReceived', [
